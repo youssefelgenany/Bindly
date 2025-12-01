9a73da62c1cc9110be3bd5b930b55135e7c892c5
youssefelgenany <youssefkhaled1204@gmail.com>
2025-12-01 22:28:58 +0200

    commit

--- frontend/src/pages/VendorMyRequests.jsx
+++ frontend/src/pages/VendorMyRequests.jsx
@@ -606,7 +606,7 @@ const VendorMyRequests = () => {
                 margin: 0,
                 animation: 'slideInRight 0.8s ease-out 0.2s both'
               }}>
-                View all requests for upcoming bazaars or booth setups you want to participate in (pending or rejected).
+                View all requests for upcoming bazaars or booth setups you want to participate in.
               </p>
             </div>
           </div>
